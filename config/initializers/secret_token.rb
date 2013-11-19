2b31511c50d0ba2f4907aee2daa40326d379cabd
Daniel Wade <1danwade@gmail.com>
2013-11-19 01:01:41 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetstreamRuby1931::Application.config.secret_token = 'cc37eb401a51947ddc51b0e82ddaab11ef7158d1d50314de57eb85ed289809f5b8db5acfee5eaf85759c0928302121f85ed5b80c35925d4aad4d912f1c986224'
