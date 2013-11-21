0f0b178f07fb3b75d4acf352e708d05c056ff155
Daniel Wade <1danwade@gmail.com>
2013-11-21 15:40:37 -0500

    css + bootstrap

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -1,7 +1,7 @@
 var url = location.host.replace(location.port, window.StandaloneServerPort) + '/websocket',
     dispatcher = new WebSocketRails(url),
     chan = dispatcher.subscribe('twitter'),
-    text, tweets, topTweets;
+    text, tweets, topTweets, tweet, ul, li, tweet_container, tweet_text, tweet_count, tweet_author;
 
 var Tweet = function Tweet(tweet, placement) {
   this.retweetCount = tweet.retweet_count;
@@ -14,21 +14,36 @@ Tweet.prototype.toString = function() {
   return this.name + ' :: ' + this.text + ' [' + this.retweetCount + '] :: ' + this.placement;
 }
 
+Tweet.prototype.toHtml = function() {
+  var container, text, retweetCount, author, tweetMeta;
+  container = $('<li class="Tweet list-group-item">');
+  text = $('<blockquote">').append($('<p>', {class: 'Tweet-text', text: this.text}));
+  tweetMeta = $('<small>');
+  author = $('<cite>', {class: 'Tweet-author', text: this.name});
+  retweetCount = $('<span>', {class: 'Tweet-count badge', text: this.retweetCount});
+  tweetMeta.append(author).append(retweetCount);
+  text.append(tweetMeta);
+  return container.append(text);
+}
+
 // top 10 (incoming)
 chan.bind('track_top10', function(data) {
+  var tweetsContainer;
+  tweetsContainer = $('<ul class="top-tweets list-group">');
   data = JSON.parse(data); // keys: top_ten, track, filter_change
-  topTweets = $.map(data.top_ten, function(tweet, idx) {
-    var tweet = new Tweet(tweet, idx+1);
-    return $('<p>', {class: 'top-tweet', text: tweet.toString()});
+  $.each(data.top_ten, function(idx, tweet) {
+    tweet = new Tweet(tweet, idx+1);
+    tweetsContainer.append(tweet.toHtml()); // respects order
   });
-  $('#current-filter').val(data.track);
-  $('#tweets').html(topTweets);
+  $('.js-currentFilter').text(data.track);
+  $('.js-tweets').html(tweetsContainer);
 });
 
 // filter change (outgoing)
 $(function() {
   // todo: disable submit for delay to avoid twitter throttling with too frequent track changes
-  $('.submit').click(function() {
+  $('.js-updateFilter').submit(function() {
     dispatcher.trigger('twitter.filter', {new_track: $('#current-filter').val()});
+    return false;
   });
 });
